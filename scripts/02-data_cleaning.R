d596cc269a1c71f4cb01d5d64e14a0a492e50078
Maroosh-Gillani <maroosh1171@gmail.com>
2024-01-19 23:33:34 -0500

    adding cleaned data as a csv file. Cleaned data contains only columns with Auto theft and Bike theft info

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,44 +1,36 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Cleans the raw data recorded by two observers..... [...UPDATE THIS...]
+# Author: Maroosh Gillani
+# Date: 19 January 2024
+# Contact: maroosh.gillani@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
 
 #### Workspace setup ####
 library(tidyverse)
+library(dplyr)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+raw_data <- read_csv("inputs/data/raw_data.csv")
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+# Select the columns AUTOTHEFT_2014 - AUTOTHEFT_2023 and BIKETHEFT_2014 - BIKETHEFT_2023 only
+columns_to_keep <- c(
+  paste0("AUTOTHEFT_", 2014:2023),
+  paste0("BIKETHEFT_", 2014:2023)
+)
+
+# Getting rid of any rows with NA values
+cleaned_data <- raw_data %>%
+  select(all_of(columns_to_keep)) %>%
+  drop_na()
+
+# Sum up the data for each column
+sum_row <- cleaned_data %>%
+  summarise(across(everything(), sum))
+
+# Add the sum row to the cleaned_data
+cleaned_data <- bind_rows(cleaned_data, sum_row)
 
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+write_csv(cleaned_data, "outputs/data/cleaned_data.csv")
+
